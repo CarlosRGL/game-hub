72152c6daa64fadd2e0e7220eef98ecb99d66802
CarlosRGL <carlos@quai13.com>
2023-11-24 13:47:27 +0100

    Refactor GameGrid component and useGames hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,15 +1,20 @@
+import { SimpleGrid } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
+import GameCard from './GameCard';
 
 function GameGrid() {
-  const { games, error } = useGames();
+  const { games } = useGames();
   return (
     <>
-      <ul>
-        {error && <li>{error.message}</li>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding={10}
+        spacing={10}
+      >
         {games?.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard key={game.id} game={game} />
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 }
