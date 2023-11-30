c9af9abeaf46d4fcd47e63f62ba0dfe0858f6278
CarlosRGL <carlos@quai13.com>
2023-11-30 09:43:14 +0100

    Refactor code to use Skeleton components for loading state

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,39 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonText,
+  Text,
+} from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
 
 function GenreList() {
   const { data, loading } = useGenres();
+  const skeletons = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
 
   if (loading) {
-    return <Spinner />;
+    return (
+      <List>
+        {skeletons.map((skeleton) => (
+          <ListItem key={skeleton} paddingY={'5px'}>
+            <HStack>
+              <Skeleton
+                height={'32px'}
+                width={'32px'}
+                borderRadius={'8px'}
+                flexShrink={0}
+              />
+              <SkeletonText noOfLines={2} height={'30px'} width={'100%'} />
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    );
   }
   return (
     <List>
