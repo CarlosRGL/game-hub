0b15186809324ab02f1f00472f4eaa83a901a259
CarlosRGL <carlos@quai13.com>
2024-01-17 20:54:57 +0100

    Add sort order functionality to SortSelector component

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,16 +1,35 @@
 import { Menu, MenuButton, Button, MenuList, MenuItem } from '@chakra-ui/react';
 import { BsChevronDown } from 'react-icons/bs';
 
-function SortSelector() {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+function SortSelector({ onSelectSortOrder, sortOrder }: Props) {
+  const sortOrders = [
+    { value: 'relevance', label: 'relevance' },
+    { value: '-added', label: 'release date' },
+    { value: 'name', label: 'name ' },
+    { value: '-released', label: 'release date' },
+    { value: '-metacritic', label: 'metacritic' },
+    { value: '-rating', label: 'rating' },
+  ];
+  const currentSortOrder = sortOrders.find((o) => o.value === sortOrder);
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Order by relevance
+        Order by {currentSortOrder?.label || 'relevance'}
       </MenuButton>
       <MenuList>
-        <MenuItem>Order by relevance</MenuItem>
-        <MenuItem>Order by release date</MenuItem>
-        <MenuItem>Order by rating</MenuItem>
+        {sortOrders.map((order) => (
+          <MenuItem
+            onClick={() => onSelectSortOrder(order.value)}
+            key={order.value}
+          >
+            {order.label}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
